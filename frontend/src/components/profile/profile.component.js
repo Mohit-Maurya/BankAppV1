a418efdd9d88008ccb50ee91221c636e28a8e36d
sweety879 <seelasweety38@gmail.com>
2022-08-03 20:25:17 +0530

    few styling

--- frontend/src/components/profile/profile.component.js
+++ frontend/src/components/profile/profile.component.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { IoCloseCircleOutline } from "react-icons/io5";
+import { IoCloseCircleOutline ,IoCallSharp,IoMail,IoHome} from "react-icons/io5";
 import axios from 'axios';
 
 function Profile() {
@@ -10,7 +10,7 @@ function Profile() {
     })
 
     const [update, setUpdate] = useState(false)
-  
+
     const getUser = async () => {
         await axios.get("http://localhost:8080/user/" + (1122).toString())
             .then((res) => {
@@ -26,28 +26,27 @@ function Profile() {
             })
     }
 
-    const updateProfile = async (event)=>{
+    const updateProfile = async (event) => {
         event.preventDefault();
-        await axios.put("http://localhost:8080/user/" + (1122).toString(),data)
-                .then((res)=>{
-                   if(res.status===200)
-                    {
-                        window.location.reload(false); 
-                        alert("successfully updated!!")
-                    }
-                })
-                .catch((err)=>
-                        console.log(err))
+        await axios.put("http://localhost:8080/user/" + (1122).toString(), data)
+            .then((res) => {
+                if (res.status === 200) {
+                    window.location.reload(false);
+                    alert("successfully updated!!")
+                }
+            })
+            .catch((err) =>
+                console.log(err))
     }
 
-    const cancelUpdate = async (event)=>{ 
+    const cancelUpdate = async (event) => {
         if (window.confirm("Changes will not be saved") === true) {
-            window.location.reload(false); 
-          } 
+            window.location.reload(false);
+        }
         event.preventDefault();
     }
-   
-    
+
+
     useEffect(() => {
         getUser()
     }, [])
@@ -55,54 +54,61 @@ function Profile() {
 
     return (
         <div>
-           
-        <div className="card mx-auto mt-5" style={{ width: '50vw' }}>
-            <div className="card-header">
-                <h5 className="float-start">Profile</h5>
-               {update? <button onClick={cancelUpdate} className='float-end' style={{borderColor:'transparent',backgroundColor:'transparent'}}>
-                                    <IoCloseCircleOutline size='2rem' color='red'/></button> :
-                        <button className='btn btn-primary float-end' onClick={() => {setUpdate(true);}}>Update</button>
-                }
-            </div>
-            <div className="card-body">
-                <div className="col-6 form-group mb-3">
-                    <label>Name </label>
-                    <input type="text" className="form-control" id="name" defaultValue={data.name} onChange={(e) => setData((prevState) => ({ ...prevState, name: e.target.value }))} disabled={!update} />
+            
+            <div className="card mx-auto mt-5" style={{ width: '50vw' }}>
+                <div className="card-header">
+                    <h5 className="float-start">Profile</h5>
+                    {update ? <button onClick={cancelUpdate} className='float-end' style={{ borderColor: 'transparent', backgroundColor: 'transparent' }}>
+                        <IoCloseCircleOutline size='2rem' color='red' /></button> :
+                        <button className='btn btn-primary float-end' onClick={() => { setUpdate(true); }}>Update</button>
+                    }
                 </div>
-                <div className='row'>
+                <div className="card-body">
                     <div className="col-6 form-group mb-3">
-                        <label>PanCard Number </label>
-                        <input type="text" className="form-control" id="pancard" defaultValue={data.pan} onChange={(e) => setData((prevState) => ({ ...prevState, pan: e.target.value }))} disabled={!update} />
+                        <label><b>Name </b></label>
+                        {update ? <input type="text" className="form-control" id="name" defaultValue={data.name} onChange={(e) => setData((prevState) => ({ ...prevState, name: e.target.value }))} disabled={!update} /> :
+                            <p className='ms-3'> {data.name}</p>}
                     </div>
-                    <div className="col-6 form-group mb-3">
-                        <label>Aadhaar Number</label>
-                        <input type="text" className="form-control" id="aadhaar" defaultValue={data.aadhaarNumber} onChange={(e) => setData((prevState) => ({ ...prevState, aadhaarNumber: e.target.value }))} disabled={!update} />
+                    <div className='row'>
+                        <div className="col-6 form-group mb-3">
+                            <label><b>PanCard Number </b></label>
+                            {update ? <input type="text" className="form-control" id="pancard" defaultValue={data.pan} onChange={(e) => setData((prevState) => ({ ...prevState, pan: e.target.value }))} disabled={!update} /> :
+                                <p className='ms-3'>  {data.pan}</p>}
+                        </div>
+                        <div className="col-6 form-group mb-3">
+                            <label><b>Aadhaar Number</b></label>
+                            {update ? <input type="text" className="form-control" id="aadhaar" defaultValue={data.aadhaarNumber} onChange={(e) => setData((prevState) => ({ ...prevState, aadhaarNumber: e.target.value }))} disabled={!update} /> :
+                                <p className='ms-3'>  {data.aadhaarNumber}</p>}
+                        </div>
                     </div>
-                </div>
-                <div className='row'>
-                    <div className="col-6 form-group mb-3">
-                        <label>Mobile Number </label>
-                        <input type="text" className="form-control" id="mobile" defaultValue={data.mobileNumber} onChange={(e) => setData((prevState) => ({ ...prevState, mobileNumber: e.target.value }))} disabled={!update} />
+                    <div className='row'>
+                        <div className="col-6 form-group mb-3">
+                            <label><IoCallSharp className='mb-1'/> <b>Mobile Number  </b></label>
+                            {update ? <input type="text" className="form-control" id="mobile" defaultValue={data.mobileNumber} onChange={(e) => setData((prevState) => ({ ...prevState, mobileNumber: e.target.value }))} disabled={!update} />
+                                : <p className='ms-3'>  {data.mobileNumber}</p>}
+                        </div>
+                        <div className="col-6 form-group mb-3">
+                            <label><IoMail className='mb-1'/> <b> Email </b></label>
+                            {update ? <input type="text" className="form-control" id="email" defaultValue={data.email} onChange={(e) => setData((prevState) => ({ ...prevState, email: e.target.value }))} disabled={!update} />
+                                : <p className='ms-3'> {data.email}</p>}
+                        </div>
                     </div>
-                    <div className="col-6 form-group mb-3">
-                        <label>Email</label>
-                        <input type="text" className="form-control" id="email" defaultValue={data.email} onChange={(e) => setData((prevState) => ({ ...prevState, email: e.target.value }))} disabled={!update} />
+                    <div className="col-11 form-group mb-3">
+                        <label><IoHome className='mb-1'/> <b> Permanent Address </b> </label>
+                        {update ? <input type="text" className="form-control" id="permanentAddress" defaultValue={data.permanentAddress} onChange={(e) => setData((prevState) => ({ ...prevState, permanentAddress: e.target.value }))} disabled={!update} />
+                                 : <p className='ms-3'> {data.permanentAddress}</p>}
                     </div>
+                    <div className="col-11 form-group mb-3">
+                        <label><IoHome className='mb-1'/> <b> Temporary Address </b> </label>
+                        {update ? <input type="text" className="form-control" id="temporaryAddress" defaultValue={data.temporaryAddress} onChange={(e) => setData((prevState) => ({ ...prevState, temporaryAddress: e.target.value }))} disabled={!update} />
+                                : <p className='ms-3'> {data.temporaryAddress}</p>}
+                    </div>
+                    <br />
+                    {/* <p><span style={{ color: 'red' }}>{login.error}</span></p> */}
+                    {update && <button className="btn btn-primary float-end" onClick={updateProfile} >Save</button>}
                 </div>
-                <div className="col-11 form-group mb-3">
-                    <label>Permanent Address </label>
-                    <input type="text" className="form-control" id="permanentAddress" defaultValue={data.permanentAddress} onChange={(e) => setData((prevState) => ({ ...prevState, permanentAddress: e.target.value }))} disabled={!update} />
-                </div>
-                <div className="col-11 form-group mb-3">
-                    <label>Temporary Address </label>
-                    <input type="text" className="form-control" id="temporaryAddress" defaultValue={data.temporaryAddress} onChange={(e) => setData((prevState) => ({ ...prevState, temporaryAddress: e.target.value }))} disabled={!update} />
-                </div>
-                <br />
-                {/* <p><span style={{ color: 'red' }}>{login.error}</span></p> */}
-                {update && <button className="btn btn-primary float-end" onClick={updateProfile} >Save</button>}
             </div>
         </div>
-     </div>
     )
 }
 
