ce400aa8e396d4537f4f183336cc756a557bced7
supriya-anand-coindcx <supriya.anand@coindcx.com>
2022-08-14 21:12:12 +0530

    fixed navigation links alignment

--- frontend/src/components/navigation/nav.component.js
+++ frontend/src/components/navigation/nav.component.js
@@ -21,62 +21,67 @@ function Navigation() {
     <nav className="navbar navbar-expand-lg navbar-dark" style={headerStyles}>  
       <div className="container-fluid">
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <h1 className="navbar-nav" style={{color: "#fff"}}>404 Bank</h1>
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <div className="parent collapse navbar-collapse" id="navbarSupportedContent">
+          
+          <div className="leftChild"> 
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              {
+                !user.loggedin &&
+                  <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="/">
+                      Login
+                    </a>
+                  </li>
+              }
+
             {
-               !user.loggedin &&
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">
-                    Login
-                  </a>
+                !user.loggedin &&
+                  <li className="nav-item">
+                    <a
+                      className="nav-link active"
+                      aria-current="page"
+                      href="/registration"
+                    >
+                      Registration
+                    </a>
                 </li>
             }
 
-           {
-              !user.loggedin &&
-                <li className="nav-item">
-                  <a
-                    className="nav-link active"
-                    aria-current="page"
-                    href="/registration"
-                  >
-                    Registration
-                  </a>
-              </li>
-           }
+              {
+                user.loggedin &&
+                    <li className="nav-item">
+                      <button className="nav-link active"  aria-current="page" onClick={()=>{navigate("/profile", { state: { userid: user.userid } });}}
+                      style={{backgroundColor:"transparent",border:"none"}}>
+                          Profile
+                      </button>
+                    </li>             
+              }
 
-            {
-               user.loggedin &&
-                  <li className="nav-item">
-                    <button className="nav-link active"  aria-current="page" onClick={()=>{navigate("/profile", { state: { userid: user.userid } });}}
-                     style={{backgroundColor:"transparent",border:"none"}}>
-                        Profile
-                    </button>
-                  </li>             
-            }
+              {
+                user.loggedin &&
+                    <li className="nav-item">
+                      <button className="nav-link active"  aria-current="page" onClick={()=>{navigate("/accounts", { state: { userid: user.userid } });}}
+                      style={{backgroundColor:"transparent",border:"none"}}>
+                          Accounts
+                      </button>
+                    </li>             
+              }
 
-            {
-               user.loggedin &&
-                  <li className="nav-item">
-                    <button className="nav-link active"  aria-current="page" onClick={()=>{navigate("/accounts", { state: { userid: user.userid } });}}
-                     style={{backgroundColor:"transparent",border:"none"}}>
-                        Accounts
-                    </button>
-                  </li>             
-            }
+              {
+                user.loggedin &&
+                    <li className="nav-item">
+                      <button className="nav-link active"  aria-current="page" onClick={()=>{dispatch(log_out());console.log(user);navigate("/");}}
+                      style={{backgroundColor:"transparent",border:"none"}}>
+                          LogOut
+                      </button>
+                    </li>             
+              }
 
-            {
-               user.loggedin &&
-                  <li className="nav-item">
-                    <button className="nav-link active"  aria-current="page" onClick={()=>{dispatch(log_out());console.log(user);navigate("/");}}
-                     style={{backgroundColor:"transparent",border:"none"}}>
-                        LogOut
-                    </button>
-                  </li>             
-            }
-
-          </ul>
+            </ul>
+          </div>
+          <div className="rightChild">
+            <h2 className="navbar-nav" style={{color: "#fff"}}>404 Bank</h2>
+          </div>
         </div>
       </div>
     </nav>
